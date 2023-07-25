fa4ef4cbb164538793e3d4fad8eb414bd334a30c
Aleksandr Demidovich <demid.kbr@mail.ru>
2023-07-25 22:36:02 +0300

    feat: add swagger docs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './modules/users/users.module';
 import { TracksModule } from './modules/tracks/tracks.module';
+
 @Module({
   imports: [UsersModule, TracksModule],
   controllers: [],
--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,20 @@ import * as dotenv from 'dotenv';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as fs from 'fs';
+import * as swaggerUi from 'swagger-ui-express';
+import { parse } from 'yaml'
 
 dotenv.config();
 const PORT = process.env.PORT;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const file = fs.readFileSync('./doc/api.yaml', 'utf8');
+  const swaggerDocument = parse(file);
+
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
